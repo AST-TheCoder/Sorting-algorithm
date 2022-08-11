877bab1be5c3ae4086d941af8f05c417df74b3b4
AST-TheCoder <tonmoy.cse5.bu@gmail.com>
2022-08-12 01:43:00 +0600

    Added

--- ast_sort_three.h
+++ ast_sort_three.h
@@ -3,39 +3,41 @@ using namespace std;
 
 #define ll long long int
 
-ll next_index;
+ll next_index,val_len;
 
 struct node_three{
-    int val=0;
-    node_three* child[10]={NULL};
+    int val=0,chk=0;
+    node_three* child[10];
 };
 
-void build_trie(node_three* root,ll arr[],ll n){
+void build_trie(node_three* root,ll arr[],ll length[],ll n){
     node_three* temp_node;
     for(ll i=0;i<n;i++){
-        ll val=0;
-
-        while(arr[i]){
-            val=val*10+arr[i]%10;
-            arr[i]/=10;
-        }
-
+        ll val=arr[i];
+        ll len=val_len-length[i];
         temp_node=root;
-        while(val){
-            ll v=val%10;
-            val/=10;
-            if(temp_node->child[v]==NULL) temp_node->child[v]=new node_three();
+        while(length[i]){
+            ll v=0;
+            if(!len){
+                v=val%10;
+                val/=10;
+                length[i]--;
+            }
+            else len--;
+            if(!(temp_node->chk&(1<<v))){
+                temp_node->child[v]=new node_three();
+                temp_node->chk|=(1<<v);
+            }
             temp_node=temp_node->child[v];
-            if(!val) temp_node->val++;
+            if(!length[i]) temp_node->val++;
         }
     }
 }
 
 void sort_array(node_three* root,ll arr[],ll val){
-
     while((root->val)--) arr[next_index++]=val;
     for(ll i=0;i<10;i++){
-        if(root->child[i]==NULL) continue;
+        if(!(root->chk&(1<<i))) continue;
         sort_array(root->child[i],arr,val*10+i);
     }
 }
@@ -43,7 +45,29 @@ void sort_array(node_three* root,ll arr[],ll val){
 void ast_sort_three(ll arr[],ll n){
     node_three* root=new node_three();
     next_index=0;
+    ll length[n];
 
-    build_trie(root,arr,n);
+    for(ll i=0;i<n;i++){
+        ll val=arr[i],len=0;
+        arr[i]=0;
+        while(val){
+            arr[i]=arr[i]*10+val%10;
+            val/=10;
+            len++;
+        }
+        val_len=max(val_len,len);
+        length[i]=len;
+    }
+
+    clock_t start_time,end_time;
+    start_time=clock();
+    build_trie(root,arr,length,n);
+    end_time=clock();
+    double time_taken=double(end_time-start_time)/double(CLOCKS_PER_SEC);
+    cout<<time_taken<<endl;
+    start_time=clock();
     sort_array(root,arr,0);
+    end_time=clock();
+    time_taken=double(end_time-start_time)/double(CLOCKS_PER_SEC);
+    cout<<time_taken<<endl;
 }
